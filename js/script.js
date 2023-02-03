6e9e3d9c2c4cd87b4d616ce9a6b804b1e4574d35
Dina <sharipova0308@gmail.com>
2023-02-03 22:00:20 +0500

    add scroll

--- js/script.js
+++ js/script.js
@@ -21,11 +21,26 @@ window.addEventListener('DOMContentLoaded', () => {
         $(this).hover(function() {
             $('.prod__overlay').eq(i).fadeOut();
             $('.prod__button').eq(i).addClass('prod__button_active');
-            $('.prod__title').eq(i).addClass ('prod__title_active')
+            $('.prod__title').eq(i).addClass ('prod__title_active');
         }, function(){
             $('.prod__overlay').eq(i).fadeIn();
             $('.prod__button').eq(i).removeClass('prod__button_active');
-            $('.prod__title').eq(i).removeClass ('prod__title_active')
-        })
+            $('.prod__title').eq(i).removeClass ('prod__title_active');
+        });
     });
+
+     //плавный скролл
+ $('.scrollto a').on('click', function() {
+
+    let href = $(this).attr('href');
+  
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, {
+        duration: 370,   // по умолчанию «400» 
+        easing: "linear" // по умолчанию «swing» 
+    });
+  
+    return false;
+  });
 });
